40411cec491c43acfc8aa8ff0352eee4a0ebb045
sourabhpande532 <sourabhpande43@gmail.com>
2025-12-08 15:54:32 +0530

    Add: Perform CRUD with leads

--- server/routes/lead.js
+++ server/routes/lead.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express.Router();
 const SalesLead = require("../model/SalesLead");
 const SalesAgent = require("../model/SalesAgent");
+const { default: mongoose } = require("mongoose");
 
 const createNewLead = async (newLead) => {
   try {
@@ -37,36 +38,129 @@ app.post("/", async (req, res) => {
     console.error("Internal server error /lead", error);
     res.status({
       success: false,
-      message: "Internal server error create lead."
+      message: "Internal server error create lead.",
     });
   }
 });
 
-const fetchedAllLeads = async () => {
-  try {
-    const allLeads = await SalesLead.find().populate("salesAgent");
-    return allLeads;
-  } catch (error) {
-    console.error("server error /lead/get", error.message);
-  }
-};
-
 app.get("/", async (req, res) => {
   try {
-    const leads = await fetchedAllLeads();
-    if (leads.length > 0) {
-      res.status(200).json({ success: true, data: { leads } });
-    } else {
-      res.status(404).json({ success: false, message: "leads not found." });
+    const { salesAgent, status, tags, source, sortBy, sortDir } = req.query;
+    const filter = {};
+    if (salesAgent) filter.salesAgent = salesAgent;
+    // Validate status only if user passed status
+    if (status) {
+      const validStatus = [
+        "New",
+        "Contacted",
+        "Qualified",
+        "Proposal Sent",
+        "Closed",
+      ];
+      if (!validStatus.includes(status)) {
+        return res.status(400).json({
+          success: false,
+          error:
+            "Invalid input: 'status' must be one of ['New','Contacted','Qualified','Proposal Sent','Closed'].",
+        });
+      }
+      filter.status = status;
     }
-  } catch (error) {
-    console.error("Internal server error `/fetch/leads`", error.message);
-    res.status(500).json({
-      success: false,
-      message: "Internal error /get/leads",
-      err: error.message,
+    if (source) filter.source = source;
+    if (tags)
+      filter.tags = { $all: Array.isArray(tags) ? tags : tags.split(",") };
+    let query = SalesLead.find(filter).populate({
+      path: "salesAgent",
+      select: "name email",
     });
+
+    if (sortBy) {
+      const dir = sortDir === "desc" ? -1 : 1;
+      const sortObj = {};
+      sortObj[sortBy] = dir;
+      query = query.sort(sortObj);
+    }
+    // return a real promis .exec()
+    const leads = await query.exec();
+    res.json({ success: true, data: { leads } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    const lead = await SalesLead.findById(req.params.id).populate({
+      path: "salesAgent",
+      select: "name email",
+    });
+    if (!lead)
+      return res
+        .status(404)
+        .json({ error: `Lead with ID ${req.params.id} not found` });
+    res.status(200).json({ success: true, data: { lead } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error /lead" });
+  }
+});
+
+// UPDATE:
+app.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: `Invalid Lead ID '${id}'.` });
+    }
+
+    const lead = await SalesLead.findById(id);
+    if (!lead)
+      return res.status(404).json({ error: `Lead with ID '${id}' not found.` });
+    if (updates.salesAgent) {
+      if (!mongoose.Types.ObjectId.isValid(updates.salesAgent)) {
+        return res.status(400).json({
+          error: `Invalid Sales Agent ID '${updates.salesAgent}'.`,
+        });
+      }
+    }
+    Object.assign(lead, updates);
+    await lead.save();
+    await lead.populate({ path: "salesAgent", select: "name" });
+    res.status(200).json({ success: true, data: { lead } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error /lead/update" });
+  }
+});
+
+// DELETE
+app.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: `Lead with ID '${id}' not found.` });
+    }
+    const lead = await SalesLead.findByIdAndDelete(req.params.id);
+    lead
+      ? res
+          .status(200)
+          .json({ success: true, message: "Lead deleted successfully." })
+      : res.json({
+          success: false,
+          error: `lead with Id ${req.params.id} not found.`,
+        });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Sever error /delete/lead" });
   }
 });
 
 module.exports = app;
+
+/* NOTE: 
+?tags=Follow-up return array && 1st conditio true o.w ?tags=Follow-up,High value 2nd condtion true. 
+$all checks that a field contains all the values inside an array.
+*/
