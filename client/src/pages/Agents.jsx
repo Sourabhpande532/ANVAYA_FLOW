e5ea7ed3c9893e17b4748d2d9973f14bda2c9522
sourabhpande532 <sourabhpande43@gmail.com>
2026-04-01 22:00:15 +0530

    fix : deployment eslint issue

--- client/src/pages/Agents.jsx
+++ client/src/pages/Agents.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, useContext } from "react";
 import { fetchJSON } from "../api";
 import { LeadContext } from "../context/LeadContext";
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchJSON } from "../api";
